3d45ef666d4c06c0b368aa679ffb20e44fc4edad
xxcclong <hkz262626@outlook.com>
2025-05-19 14:11:18 +0800

    update

---
+++ mTuner-FSDP/run_fsdp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+seq_lens=(1024 2048 4096 8192)
+models=("7" "13" "30" "70")
+
+for seq in "${seq_lens[@]}"; do
+  for model in "${models[@]}"; do
+    echo "Running training for seq_len=$seq, model=$model"
+    python3 example/fsdp_train.py \
+      train.seq_len=$seq \
+      train.batch_size=max \
+      ac=None \
+      model="/data/dataset/Llama-2-${model}b-hf" | tee fsdp_output/result_modelsize${model}_seq${seq}.txt
+  done
+done
