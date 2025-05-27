b23441bc773ed49c30308966f984536138e56b86
xxcclong <hkz262626@outlook.com>
2025-05-28 01:42:46 +0800

    modify data path

--- mTuner-FSDP/run_ds.sh
+++ mTuner-FSDP/run_ds.sh
@@ -3,13 +3,17 @@
 seq_lens=(1024 2048 4096 8192)
 models=("7" "13" "30" "70")
 
+mkdir -p ../ds_output
+mkdir -p memory_traces
+
 for seq in "${seq_lens[@]}"; do
   for model in "${models[@]}"; do
     echo "Running training for seq_len=$seq, model=$model"
-    python3 example/fsdp_train.py \
+    echo "log saving to ../fsdp_output/result_modelsize${model}_seq${seq}.txt"
+    srun --exclusive --partition=debug --nodes=1 --gres=gpu:8 -n 8 python3 example/fsdp_train.py \
       train.seq_len=$seq \
       train.batch_size=max \
-      ac=checkpoint \
-      model="/data/dataset/Llama-2-${model}b-hf" | tee ds_output/result_modelsize${model}_seq${seq}.txt
+      ac=checkpoint profile.memory=False debug.skip_comm=False shard_group_size=8 \
+      model="/public/home/hkz/repo/hkz/data/llama/${model}b" | tee ../ds_output/result_modelsize${model}_seq${seq}.txt
   done
 done
--- mTuner-FSDP/run_fsdp.sh
+++ mTuner-FSDP/run_fsdp.sh
@@ -3,13 +3,18 @@
 seq_lens=(1024 2048 4096 8192)
 models=("7" "13" "30" "70")
 
+mkdir -p ../fsdp_output
+mkdir -p memory_traces
+
 for seq in "${seq_lens[@]}"; do
   for model in "${models[@]}"; do
     echo "Running training for seq_len=$seq, model=$model"
-    python3 example/fsdp_train.py \
+    echo "log saving to ../fsdp_output/result_modelsize${model}_seq${seq}.txt"
+    srun --exclusive --partition=debug --nodes=1 --gres=gpu:8 -n 8 python3 example/fsdp_train.py \
       train.seq_len=$seq \
       train.batch_size=max \
-      ac=None \
-      model="/data/dataset/Llama-2-${model}b-hf" | tee fsdp_output/result_modelsize${model}_seq${seq}.txt
+      ac=None profile.memory=False debug.skip_comm=False shard_group_size=8 \
+      model="/public/home/hkz/repo/hkz/data/llama/${model}b" | tee ../fsdp_output/result_modelsize${model}_seq${seq}.txt
+    
   done
 done
